95014ea2f0f1837917a04e83dfde9c154c0f0d88
Bryan Moreno <abre.go@outlook.com>
2022-03-06 19:56:23 -0600

    Components and styles

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>¿Quién es este Pokémon?</h1>
+
+    <!-- TODO: img -->
+    <PokemonPicture />
+
+    <!-- TODO: Opciones -->
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from '../components/PokemonPicture.vue';
+import PokemonOptions from '../components/PokemonOptions.vue';
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions,
+    }
+}
+</script>
